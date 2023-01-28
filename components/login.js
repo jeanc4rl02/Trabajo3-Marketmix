0a71d533b33df90976888f8585a5831f42184824
jeanc4rl02 <jean.carrillo@udea.edu.co>
2023-01-28 13:17:07 -0500

    primer commit back

---
+++ components/login.js
@@ -0,0 +1,41 @@
+Vue.component("log-in", {
+  template: `
+  
+  `,
+
+  data() {
+    const usuarios = [
+      {
+        rol: "Admin",
+        nombre: "Jean",
+        apellido: "Carrillo",
+        usuario: "jeanc4rl02",
+        contraseña: "admin123",
+      },
+      {
+        rol: "Secretario",
+        nombre: "Jean",
+        apellido: "Carrillo",
+        usuario: "jeanc4rl02",
+        contraseña: "admin123",
+      },
+      {
+        rol: "Vendedor",
+        nombre: "Jean",
+        apellido: "Carrillo",
+        usuario: "jeanc4rl02",
+        contraseña: "admin123",
+      },
+      {
+        rol: "Ensamblador",
+        nombre: "Jean",
+        apellido: "Carrillo",
+        usuario: "jeanc4rl02",
+        contraseña: "admin123",
+      },
+    ];
+    return {};
+  },
+
+  methods: {},
+});
